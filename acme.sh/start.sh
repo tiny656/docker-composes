c250622a5848e06b579af2039ccd0ed6d56c68e8
RealTiny656 <tiny656@hotmail.com>
2022-12-02 09:18:01 +0800

    Update start.sh

--- acme.sh/start.sh
+++ acme.sh/start.sh
@@ -1,16 +1,19 @@
 # 1. create out folder
 mkdir -p /acme.sh/out/$MY_DOMAIN
 
-# 2. enable acme.sh auto upgrade
+# 2. set notification hook to telegram (https://github.com/acmesh-official/acme.sh/wiki/notify)
+acme.sh --set-notify --notify-hook telegram --notify-level 2 --notify-mode 1
+
+# 3. enable acme.sh auto upgrade
 acme.sh --upgrade --auto-upgrade
 
-# 3. register account with zerossl
-acme.sh --register-account -m $MY_EMAIL
+# 4. register account with zerossl
+acme.sh --register-account -m tiny656@hotmail.com # <your email>
 
-# 4. issue cert by dns_azure way
+# 5. issue cert by dns_azure way
 acme.sh --issue --dns dns_azure -d $MY_DOMAIN -d *.$MY_DOMAIN
 
-# 5. save cert results into `out` folder
+# 6. save cert results into `out` folder
 acme.sh --install-cert -d $MY_DOMAIN \
 --cert-file      /acme.sh/out/$MY_DOMAIN/$MY_DOMAIN.cer \
 --ca-file        /acme.sh/out/$MY_DOMAIN/ca.cer \
