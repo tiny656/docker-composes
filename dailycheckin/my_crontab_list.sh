f975aab6533e6f7b5f42d451dd4807a480f485f1
tiny656 <tiny656@hotmail.com>
2022-12-05 13:30:10 +0800

    add dailycheckin

---
+++ dailycheckin/my_crontab_list.sh
@@ -0,0 +1,9 @@
+#必须要的默认定时任务请勿删除
+0 */12 * * * sh /dailycheckin/default_task.sh |ts >> /dailycheckin/logs/default_task.log 2>&1
+# 每天的 23:50 分清理一次日志
+50 23 */2 * * rm -rf /dailycheckin/logs/*.log
+
+
+##############每日签到一次任务##############
+# 每日签到(every 8 hours 执行一次)
+45 */8 * * * cd /dailycheckin && dailycheckin |ts >> /dailycheckin/logs/dailycheckin.log 2>&1
