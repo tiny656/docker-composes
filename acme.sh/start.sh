f849a38eebb79d0d23f4325fe9f6d150cdc4f95f
RealTiny656 <tiny656@hotmail.com>
2023-02-06 08:01:16 +0800

    Update start.sh

--- acme.sh/start.sh
+++ acme.sh/start.sh
@@ -10,7 +10,7 @@ acme.sh --upgrade --auto-upgrade
 # 4. register account with zerossl
 acme.sh --register-account -m $MY_EMAIL
 
-# 4. issue cert by dns_azure way, install cert results into `out` folder
+# 4. issue cert by dns_azure way, and register the renew hook
 # 4.1 issue cert by dns_azure way
 acme.sh --issue --dns dns_azure -d $MY_DOMAIN -d *.$MY_DOMAIN -k ec-256 \
 --renew-hook "acme.sh --install-cert -d $MY_DOMAIN \
@@ -26,3 +26,10 @@ acme.sh --issue --dns dns_azure -d $MY_DOMAIN -d *.$MY_DOMAIN -k ec-256 \
 # --key-file       /acme.sh/out/$MY_DOMAIN/$MY_DOMAIN.key \
 # --fullchain-file /acme.sh/out/$MY_DOMAIN/fullchain.cer"
 # you can also try with letsencrypt, add `--server letsencrypt` behind
+
+# 5. install cert results into `out` folder
+acme.sh --install-cert -d $MY_DOMAIN \
+--cert-file      /acme.sh/out/$MY_DOMAIN/$MY_DOMAIN.cer \
+--ca-file        /acme.sh/out/$MY_DOMAIN/ca.cer \
+--key-file       /acme.sh/out/$MY_DOMAIN/$MY_DOMAIN.key \
+--fullchain-file /acme.sh/out/$MY_DOMAIN/fullchain.cer
